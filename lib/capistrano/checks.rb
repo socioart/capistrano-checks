32cb5b19482258995459903f42b6054cd955484b
labocho <labocho@penguinlab.jp>
2024-02-22 13:25:48 +0900

    initial commit

---
+++ lib/capistrano/checks.rb
@@ -0,0 +1,8 @@
+require "capistrano/checks/version"
+
+module Capistrano
+  module Checks
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/checks/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Checks
+    VERSION = "0.1.0"
+  end
+end
